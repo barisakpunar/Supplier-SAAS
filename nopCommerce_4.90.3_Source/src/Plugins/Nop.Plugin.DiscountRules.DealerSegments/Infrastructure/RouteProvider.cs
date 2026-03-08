427533815c83f30827372c350dac24eea2146cc3
Barış <barisakpunar@hotmail.com>
2026-03-08 21:52:53 +0300

    feat(discounts): add dealer segment requirement rule

---
+++ nopCommerce_4.90.3_Source/src/Plugins/Nop.Plugin.DiscountRules.DealerSegments/Infrastructure/RouteProvider.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+using Nop.Web.Framework;
+using Nop.Web.Framework.Mvc.Routing;
+
+namespace Nop.Plugin.DiscountRules.DealerSegments.Infrastructure;
+
+/// <summary>
+/// Represents plugin route provider
+/// </summary>
+public class RouteProvider : IRouteProvider
+{
+    /// <summary>
+    /// Register routes
+    /// </summary>
+    /// <param name="endpointRouteBuilder">Route builder</param>
+    public void RegisterRoutes(IEndpointRouteBuilder endpointRouteBuilder)
+    {
+        endpointRouteBuilder.MapControllerRoute(name: DiscountRequirementDefaults.ConfigurationRouteName,
+            pattern: "Admin/DiscountRulesDealerSegments/Configure",
+            defaults: new { controller = "DiscountRulesDealerSegments", action = "Configure", area = AreaNames.ADMIN });
+    }
+
+    /// <summary>
+    /// Gets a priority of route provider
+    /// </summary>
+    public int Priority => 0;
+}
---
+++ nopCommerce_4.90.3_Source/src/Plugins/Nop.Plugin.DiscountRules.DealerSegments/Models/RequirementModel.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Nop.Web.Framework.Mvc.ModelBinding;
+
+namespace Nop.Plugin.DiscountRules.DealerSegments.Models;
+
+public class RequirementModel
+{
+    public RequirementModel()
+    {
+        AvailableDealerSegments = new List<SelectListItem>();
+    }
+
+    [NopResourceDisplayName("Plugins.DiscountRules.DealerSegments.Fields.DealerSegment")]
+    public int DealerSegmentId { get; set; }
+
+    public int DiscountId { get; set; }
+
+    public int RequirementId { get; set; }
+
+    public IList<SelectListItem> AvailableDealerSegments { get; set; }
+}
